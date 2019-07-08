2c0d22ed6df0d56e35b8334eaae2bf28c2bafded
owner <owner@owners-iMac.local>
2019-07-08 11:39:17 -0700

    added prection model, replace variable in index

--- routes.py
+++ routes.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from sklearn.externals import joblib
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    model = joblib.load('regr_tree_1.pkl')
+    prediction = str(model.predict([[239000.0, 12, 4.0, 4772, 118, 8350.0, 0, 0]]).round(1)[0])
+    return render_template("index.html", prediction=prediction)
 
 if __name__ == ("__main__"):
     app.run(debug=True)
